953e714dc4c26633cce36286b18ffeab79c9a118
rexim <reximkut@gmail.com>
2014-11-28 02:34:49 +0600

    Bumped version for marmalade.

--- org-cliplink.el
+++ org-cliplink.el
@@ -4,7 +4,7 @@
 
 ;; Author: Alexey Kutepov <reximkut@gmail.com>
 ;; URL: http://github.com/rexim/org-cliplink
-;; Version: 0.1.1
+;; Version: 0.2
 
 ;; Permission is hereby granted, free of charge, to any person
 ;; obtaining a copy of this software and associated documentation
